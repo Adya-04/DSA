21b9a97e0764064ed50b3cc60f2d68c5fb2e4182
Adya Singh <162277207+Adya-04@users.noreply.github.com>
2025-12-09 16:34:44 +0530

    Time: 10 ms (84.16%) | Memory: 59.3 MB (37.88%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,20 +1,25 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0,1);
+        return countAtMost(nums, k) - countAtMost(nums, k - 1);
+    }
 
-        int oddCount = 0;
-        int result = 0;
-
-        for(int num: nums){
-            oddCount += num % 2;  // same as this -> if(num%2 !=0) oddCount++;
-
-            // Check if we have seen (oddCount - k)
-            result += map.getOrDefault(oddCount - k, 0);
+    //This is the same func we use in binary subarray with sum problem
+    private int countAtMost(int[] nums, int K) {
+        if (K < 0) return 0;
         
-            // Store current oddCount into the map
-            map.put(oddCount, map.getOrDefault(oddCount,0)+1);
+        int left = 0, sum = 0, count = 0;
+
+        for(int right = 0; right < nums.length; right++) {
+            sum += nums[right] % 2;
+
+            while(sum > K){
+                sum -= nums[left] % 2;
+                left++;
+            }
+
+            // All subarrays ending at right with sum ≤ K
+            count += right - left + 1;
         }
-        return result;
+        return count;
     }
 }
