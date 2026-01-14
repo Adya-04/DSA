9ae82fcf76abfd6c06e55b44996242bc16a0c3aa
Adya Singh <162277207+Adya-04@users.noreply.github.com>
2026-01-14 17:54:44 +0530

    Time: 88 ms (97.37%) | Memory: 149.2 MB (37.90%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.java
+++ 3763-separate-squares-i/separate-squares-i.java
@@ -1,45 +1,57 @@
 class Solution {
-    private int[][] squares;
-    private double totalArea;
+    
+    private boolean check(int[][] squares, double mid_y, double totalArea){
+        double bottomArea = 0.0;
 
-    // Check if area below yLine >= half of total area
-    private boolean check(double yLine) {
-        double below = 0.0;
-        for (int[] sq : squares) {
-            int y = sq[1];
-            int l = sq[2];
-            // if bottom < yLine, some part contributes to below area
-            if (y < yLine) {
-                // height of part below line (clamp to square height)
-                double h = Math.min(yLine - y, l);
-                below += h * l;
+        for(int[] square: squares){
+            double y = square[1];
+            double l = square[2];
+
+            double bot_y = y;
+            double top_y = y+l;
+
+            if(mid_y >= top_y){
+                // full square below
+                bottomArea += (l*l);
+            }
+            else if(mid_y > bot_y){
+                // partial square below
+                bottomArea += l * (mid_y-bot_y);
             }
         }
-        return below >= totalArea / 2.0;
+
+        return bottomArea >= totalArea / 2.0 ; 
     }
 
     public double separateSquares(int[][] squares) {
-        this.squares = squares;
-        totalArea = 0.0;
-        // determine search range
         double low = Double.MAX_VALUE;
-        double high = Double.MIN_VALUE;
-        for (int[] sq : squares) {
-            totalArea += (double) sq[2] * sq[2];
-            low = Math.min(low, (double) sq[1]);
-            high = Math.max(high, (double) sq[1] + sq[2]);
+        double high = -Double.MAX_VALUE;
+        double total = 0.0;
+
+        for(int[] square: squares){
+            double y = square[1];
+            double l = square[2];
+
+            total += l*l;
+            low = Math.min(y,low);
+            high = Math.max(y+l, high);
         }
 
-        // binary search for y coordinate with precision 1e-5
-        double eps = 1e-5;
-        while (high - low > eps) {
-            double mid = (low + high) / 2.0;
-            if (check(mid)) {
-                high = mid;
-            } else {
-                low = mid;
+        double resultYLine = 0.0;
+
+        while(high-low > 1e-5){
+            double midY = low + (high-low)/2.0;
+            resultYLine = midY;
+
+            if(check(squares,midY,total)){
+                // bottom area is more than half, move down
+                high = midY;
+            }
+            else{
+                low = midY;
             }
         }
-        return high;
+
+        return resultYLine;
     }
 }
