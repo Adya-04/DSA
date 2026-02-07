9ae0197b8687ec967fb238d0c97b39525462652b
Adya Singh <162277207+Adya-04@users.noreply.github.com>
2026-02-07 18:50:45 +0530

    Time: 41 ms (37.83%) | Memory: 47.9 MB (45.39%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -5,30 +5,24 @@ class Solution {
 
         int n = s.length();
         
-        int[] left_b = new int[n];
         int[] right_a = new int[n];
 
-        int count_b = 0;
-        for(int i=0; i<n; i++){
-            left_b[i] = count_b;
-            if(s.charAt(i) == 'b'){
-                count_b++;
-            }
+        int counta = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            right_a[i] = counta;
+            if (s.charAt(i) == 'a')
+                counta++;
         }
 
-        int count_a = 0;
-        for(int i=n-1; i>=0 ; i--){
-            right_a[i] = count_a;
-            if(s.charAt(i) == 'a'){
-                count_a++;
-            }
-        }
+        int count = Integer.MAX_VALUE;
+        int countb = 0;
 
-        int minCount = Integer.MAX_VALUE;
-        for(int i = 0; i<n ; i++){
-            minCount = Math.min(minCount,left_b[i]+right_a[i]);
+        for (int i = 0; i < n; i++) {
+            count = Math.min(count, countb + right_a[i]);
+            if (s.charAt(i) == 'b')
+                countb++;
         }
         
-        return minCount;
+        return count;
     }
 }
