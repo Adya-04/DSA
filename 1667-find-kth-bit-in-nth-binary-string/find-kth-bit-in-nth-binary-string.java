05a38ebdbfab0dd5a541b43158aeca9e83215d4f
Adya Singh <162277207+Adya-04@users.noreply.github.com>
2026-03-03 13:09:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (87.38%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,29 +1,25 @@
 class Solution {
     public char findKthBit(int n, int k) {
-        String s = solve(n);
-        return s.charAt(k-1);
-    }
-
-    public String solve (int i){
-        if(i==1) return "0";
-        String s = solve(i-1);
-        return s + "1" + reverse(invert(s));
-    }
-
-    public String invert(String s){
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '0'){
-                sb.append('1');
-            }
-            else{
-                sb.append('0');
-            }
+        // Base case: if n is 1, the sequence is just "0"
+        if (n == 1) {
+            return '0';
         }
-        return sb.toString();
-    }
 
-    public String reverse(String s){
-        return new StringBuilder(s).reverse().toString();
+        // Length of the sequence at level n: 2^n - 1
+        int length = (1 << n) - 1;
+
+        // If k is in the first half, recurse on the first half
+        if (k < Math.ceil(length / 2.0)) {
+            return findKthBit(n - 1, k);
+        } 
+        // If k is the middle bit, return '1'
+        else if (k == Math.ceil(length / 2.0)) {
+            return '1';
+        } 
+        // Otherwise, recurse on the mirrored second half
+        else {
+            char ch = findKthBit(n - 1, length - (k - 1)); // handle reversed
+            return (ch == '0') ? '1' : '0'; // handle flipped bit
+        }
     }
 }
