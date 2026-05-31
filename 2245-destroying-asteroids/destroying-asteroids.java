cf75665a49cbec704963f9a5d847857682678ad3
Adya Singh <162277207+Adya-04@users.noreply.github.com>
2026-06-01 01:03:44 +0530

    Time: 27 ms (30.82%) | Memory: 107.2 MB (84.30%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+
+        long totalMass = mass;
+
+        for(int asteroid : asteroids) {
+            if(totalMass < asteroid)
+                return false;
+
+            totalMass += asteroid;
+        }
+
+        return true;
+    }
+}
