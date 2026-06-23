3729951df5af1b7a4056be481fceb386342fb524
Adya Singh <162277207+Adya-04@users.noreply.github.com>
2026-06-24 02:24:09 +0530

    Time: 1 ms (100.00%) | Memory: 48.8 MB (51.55%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,36 +1,68 @@
 class Solution {
-    //Bruteforce Approach
+    //Better Approach
+    // SC = O(1)
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
 
-        int[] arr = new int[m+n];
-        int i =0, j =0,k = 0; 
+        int total = m + n;
 
-        while(i<m && j<n){
-            if(nums1[i] <= nums2[j]){
-                arr[k++] = nums1[i++];
+        int idx1 = (total / 2) - 1;
+        int element1 = -1;
+
+        int idx2 = (total / 2);
+        int element2 = -1;
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                if (k == idx1) {
+                    element1 = nums1[i];
+                }
+                if (k == idx2) {
+                    element2 = nums1[i];
+                }
+                i++;
+            } else {
+                if (k == idx1) {
+                    element1 = nums2[j];
+                }
+                if (k == idx2) {
+                    element2 = nums2[j];
+                }
+                j++;
             }
-            else{
-                arr[k++] = nums2[j++];
+
+            k++;
+        }
+
+        while (i < m) {
+            if (k == idx1) {
+                element1 = nums1[i];
             }
-        } 
-
-        while(i<m){
-            arr[k++] = nums1[i++];
+            if (k == idx2) {
+                element2 = nums1[i];
+            }
+            i++;
+            k++;
         }
 
-        while(j<n){
-            arr[k++] = nums2[j++];
+        while (j < n) {
+            if (k == idx1) {
+                element1 = nums2[j];
+            }
+            if (k == idx2) {
+                element2 = nums2[j];
+            }
+            j++;
+            k++;
         }
 
-        int total = m+n;
-
-        if(total%2 == 1) {
-            return arr[total/2];
-        }
-        else{
-            return (arr[total/2] + arr[total/2 -1])/2.0;
+        if (total % 2 == 1) {
+            return element2;
+        } else {
+            return (element1 + element2) / 2.0;
         }
     }
 }
