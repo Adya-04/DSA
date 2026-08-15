4ee5c63a404f39ef4dd2510d98b0ad42e59ddaa0
Adya Singh <162277207+Adya-04@users.noreply.github.com>
2026-08-15 16:45:05 +0530

    Time: 2 ms (32.32%) | Memory: 42.6 MB (85.59%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -8,40 +8,14 @@ class Solution {
         }
 
         while (list.size() > 1) {
-            int max1 = -1;
-            int max2 = -1;
+            Collections.sort(list);
 
-            int idx1 = -1;
-            int idx2 = -1;
+            int a = list.remove(list.size()-1);
+            int b = list.remove(list.size()-1);
 
-            // Find largest
-            for (int i = 0; i < list.size(); i++) {
-                if (max1 < list.get(i)) {
-                    max1 = list.get(i);
-                    idx1 = i;
-                }
-            }
-
-            // Find second largest
-            for (int i = 0; i < list.size(); i++) {
-                if (i != idx1 && max2 < list.get(i)) {
-                    max2 = list.get(i);
-                    idx2 = i;
-                }
-            }
-
-            list.remove(Math.max(idx1, idx2));
-            list.remove(Math.min(idx1, idx2));
-
-            if(max1 != max2){
-                list.add(max1-max2);
-            }
+            list.add(a-b);
         }
-
-        if(list.size() == 0){
-            return 0;
-        }
-
+        
         return list.get(0);
     }
 }
