b79a55a71d3742852b278387f7e8caf16aade8d9
Adya Singh <162277207+Adya-04@users.noreply.github.com>
2026-08-15 16:36:29 +0530

    Time: 3 ms (5.05%) | Memory: 42.7 MB (85.59%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,25 +1,47 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        // Create a priority queue
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-        // Add all stones to the priority queue
-        for (int stone : stones)
-            pq.add(stone);
+        ArrayList<Integer> list = new ArrayList<>();
 
-        // While there are more than 1 stone
-        while (pq.size() > 1) {
+        for (int stone : stones) {
+            list.add(stone);
+        }
 
-            // Get the two heaviest stones
-            int y = pq.poll();
-            int x = pq.poll();
+        while (list.size() > 1) {
+            int max1 = -1;
+            int max2 = -1;
 
-            // If the stones are not equal
-            if (x != y) {
-                pq.add(y - x);
+            int idx1 = -1;
+            int idx2 = -1;
+
+            // Find largest
+            for (int i = 0; i < list.size(); i++) {
+                if (max1 < list.get(i)) {
+                    max1 = list.get(i);
+                    idx1 = i;
+                }
+            }
+
+            // Find second largest
+            for (int i = 0; i < list.size(); i++) {
+                if (i != idx1 && max2 < list.get(i)) {
+                    max2 = list.get(i);
+                    idx2 = i;
+                }
+            }
+
+            list.remove(Math.max(idx1, idx2));
+            list.remove(Math.min(idx1, idx2));
+
+            if(max1 != max2){
+                list.add(max1-max2);
             }
         }
 
-        return pq.isEmpty() ? 0 : pq.poll();
+        if(list.size() == 0){
+            return 0;
+        }
+
+        return list.get(0);
     }
 }
