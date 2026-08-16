c4717f5b5231ca54ae90bf495e04fc5c0af22d0f
Adya Singh <162277207+Adya-04@users.noreply.github.com>
2026-08-16 23:42:07 +0530

    Time: 1 ms (97.57%) | Memory: 47.4 MB (40.97%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,30 +1,23 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        // Now doing this without any space
         int n = nums.length;
 
-        int[] prefixSum = new int[n];
-        prefixSum[0] = nums[0];
-
-        for(int i=1; i<n; i++){
-            prefixSum[i] = prefixSum[i-1] + nums[i];
+        int total = 0;
+        for(int i: nums){
+            total += i;
         }
-        
-        int[] suffixSum = new int[n];
-        suffixSum[n-1] = nums[n-1];
 
-        for(int i=n-2; i>=0; i--){
-            suffixSum[i] = suffixSum[i+1] + nums[i];
-        }
+        int left =0;
 
         for(int i=0; i<n; i++){
-            int left = (i==0) ? 0 : prefixSum[i-1];
-            int right = (i==n-1) ? 0 : suffixSum[i+1];
+            int right = total-left-nums[i];
 
             if(left == right){
                 return i;
             }
+            left += nums[i];
         }
-
         return -1;
     }
 }
