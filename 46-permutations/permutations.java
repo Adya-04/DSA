5da2383c67176dbc7bb271e96ff94883857be9a6
Adya Singh <162277207+Adya-04@users.noreply.github.com>
2026-08-17 12:58:36 +0530

    Time: 3 ms (6.35%) | Memory: 45.7 MB (15.46%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,30 +1,35 @@
 class Solution {
+
+    List<List<Integer>> result = new ArrayList<>();
+    HashSet<Integer> set = new HashSet<>();
+    int n;
+
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums);
+        n = nums.length;
+
+        List<Integer> temp = new ArrayList<>();
+        solve(temp,nums);
         return result;
     }
 
-    private void backtrack(List<List<Integer>> result, ArrayList<Integer> temp, int[] nums){   // If we match the length, it is a permutation
-        if(temp.size() == nums.length){
+    private void solve(List<Integer> temp, int[] nums) {
+
+        if (temp.size() == nums.length) {
             result.add(new ArrayList<>(temp));
             return;
         }
 
-        for(int n: nums){
-            // Skip if we get same element
-            if(temp.contains(n)){
-                continue;
+        for (int i=0; i<nums.length; i++) {
+
+            if (!set.contains(nums[i])) {
+                temp.add(nums[i]);
+                set.add(nums[i]);
+
+                solve(temp, nums);
+
+                set.remove(nums[i]);
+                temp.remove(temp.size() - 1);
             }
-
-            // Add the new element
-            temp.add(n);
-
-            // Go back to try other element
-            backtrack(result,temp,nums);
-
-            // Remove the element
-            temp.remove(temp.size()-1);
         }
     }
 }
